20cc5bf5f97fc8188915c1725fd288cb5bfb5bdc
Josh Buckland <josh.russell.buckland@gmail.com>
2023-03-24 15:33:49 +0000

    chore: push from KF machine

--- .config/nvim-lazy/lua/plugins/web_dev.lua
+++ .config/nvim-lazy/lua/plugins/web_dev.lua
@@ -21,70 +21,88 @@ return {
       local nls = require("null-ls")
       return {
         sources = {
-          nls.builtins.diagnostics.eslint_d.with({
-            filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
-          }),
-          nls.builtins.code_actions.eslint_d.with({
-            filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
-          }),
-          nls.builtins.formatting.prettierd.with({
-            filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "json", "jsonc" },
-          }),
-          nls.builtins.formatting.stylelint.with({
-            filetypes = { "css", "scss", "sass" },
-          }),
+          nls.builtins.diagnostics.eslint_d,
+          nls.builtins.formatting.eslint_d,
+          nls.builtins.code_actions.eslint_d,
+          nls.builtins.formatting.prettierd,
+          -- nls.builtins.formatting.stylelint,
           nls.builtins.formatting.markdownlint,
           nls.builtins.formatting.stylua,
-          nls.builtins.diagnostics.shellcheck.with({
-            extra_args = { "--severity", "warning" },
-          }),
+          nls.builtins.diagnostics.shellcheck,
+          nls.builtins.diagnostics.cfn_lint,
         },
       }
     end,
   },
-  -- add tsserver and setup with typescript.nvim instead of lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "jose-elias-alvarez/typescript.nvim",
-      init = function()
-        require("lazyvim.util").on_attach(function(_, buffer)
-          vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-          vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-        end)
-      end,
-    },
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- tsserver will be automatically installed with mason and loaded with lspconfig
-        tsserver = {},
-      },
-      -- you can do any additional lsp server setup here
-      -- return true if you don't want this server to be setup with lspconfig
-      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-      setup = {
-        -- example to setup with typescript.nvim
-        tsserver = function(_, opts)
-          require("typescript").setup({ server = opts })
-          return true
-        end,
-
-        -- Specify * to use this function as a fallback for any server
-        -- ["*"] = function(server, opts) end,
-      },
-    },
-  },
+  -- -- add tsserver and setup with typescript.nvim instead of lspconfig
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   dependencies = {
+  --     "jose-elias-alvarez/typescript.nvim",
+  --     init = function()
+  --       require("lazyvim.util").on_attach(function(_, buffer)
+  --         vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+  --         vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+  --       end)
+  --     end,
+  --   },
+  --   ---@class PluginLspOpts
+  --   opts = {
+  --     ---@type lspconfig.options
+  --     servers = {
+  --       -- tsserver will be automatically installed with mason and loaded with lspconfig
+  --       tsserver = {},
+  --     },
+  --     -- you can do any additional lsp server setup here
+  --     -- return true if you don't want this server to be setup with lspconfig
+  --     ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+  --     setup = {
+  --       -- example to setup with typescript.nvim
+  --       tsserver = function(_, opts)
+  --         require("typescript").setup({ server = opts })
+  --         return true
+  --       end,
+  --
+  --       -- Specify * to use this function as a fallback for any server
+  --       -- ["*"] = function(server, opts) end,
+  --     },
+  --   },
+  -- },
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         yamlls = {
-          schemas = {
-            ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
-            ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-            ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+          settings = {
+            yaml = {
+              format = {
+                enable = true,
+              },
+              hover = true,
+              completion = true,
+              schemaStore = {
+                enable = true,
+                url = "https://www.schemastore.org/api/json/catalog.json",
+              },
+              schemas = {
+                ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
+                ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+                ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+                -- ["https://raw.githubusercontent.com/awslabs/goformation/master/schema/cloudformation.schema.json"] = "cloud-formation.yaml",
+              },
+              customTags = {
+                "!Equals sequence",
+                "!FindInMap sequence",
+                "!GetAtt",
+                "!GetAZs",
+                "!ImportValue",
+                "!Join sequence",
+                "!Ref",
+                "!Select sequence",
+                "!Split sequence",
+                "!Sub",
+              },
+            },
           },
         },
       },
